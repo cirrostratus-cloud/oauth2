be70fd2b6f9a3fd59c4ef36c0ec7e12b74f7dec2
Patricio Ascencio <patricio.ascencio@karibu.cl>
2023-10-05 20:17:16 -0300

    feature(client): Event publisher implementation

--- client/client_event.go
+++ client/client_event.go
@@ -1,25 +1,87 @@
 package client
 
+import "github.com/cirrostratus-cloud/common/event"
+
+const (
+	ClientCreatedEventName  = "client/created"
+	ClientDisabledEventName = "client/disabled"
+	ClientEnabledEventName  = "client/enabled"
+)
+
 type ClientCreatedEvent struct {
 	ClientID string
+}
+
+func (e ClientCreatedEvent) GetPayload() map[string]interface{} {
+	return map[string]interface{}{
+		"clientID": e.ClientID,
+	}
 }
 
 type ClientDisabledEvent struct {
 	ClientID string
 }
 
+func (e ClientDisabledEvent) GetPayload() map[string]interface{} {
+	return map[string]interface{}{
+		"clientID": e.ClientID,
+	}
+}
+
 type ClientEnabledEvent struct {
 	ClientID string
+}
+
+func (e ClientEnabledEvent) GetPayload() map[string]interface{} {
+	return map[string]interface{}{
+		"clientID": e.ClientID,
+	}
 }
 
 type ClientCreatedPublisher interface {
 	ClientCreated(event ClientCreatedEvent) error
 }
 
+type clientCreatedPublisher struct {
+	eventBus event.EventBus
+}
+
+func NewClientCreatedPublisher(eventBus event.EventBus) ClientCreatedPublisher {
+	return &clientCreatedPublisher{eventBus: eventBus}
+}
+
+func (p *clientCreatedPublisher) ClientCreated(clientCreatedEvent ClientCreatedEvent) error {
+	return p.eventBus.Publish(ClientCreatedEventName, clientCreatedEvent)
+}
+
 type ClientDisabledPublisher interface {
 	ClientDisabled(event ClientDisabledEvent) error
 }
 
+type clientDisabledPublisher struct {
+	eventBus event.EventBus
+}
+
+func NewClientDisabledPublisher(eventBus event.EventBus) ClientDisabledPublisher {
+	return &clientDisabledPublisher{eventBus: eventBus}
+}
+
+func (p *clientDisabledPublisher) ClientDisabled(clientDisabledEvent ClientDisabledEvent) error {
+	return p.eventBus.Publish(ClientDisabledEventName, clientDisabledEvent)
+}
+
 type ClientEnabledPublisher interface {
 	ClientEnabled(event ClientEnabledEvent) error
+}
+
+type clientEnabledPublisher struct {
+	eventBus event.EventBus
+}
+
+func NewClientEnabledPublisher(eventBus event.EventBus) ClientEnabledPublisher {
+	return &clientEnabledPublisher{eventBus: eventBus}
+}
+
+func (p *clientEnabledPublisher) ClientEnabled(clientEnabledEvent ClientEnabledEvent) error {
+	return p.eventBus.Publish(ClientEnabledEventName, clientEnabledEvent)
 }
